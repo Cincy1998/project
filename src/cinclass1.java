3170575d9c28d335ef827c9438b6dcee378891c4
Cincy Thomas <cincythomas@most.com>
2021-10-15 13:41:19 +0530

    first commit

---
+++ src/cinclass1.java
@@ -0,0 +1,7 @@
+public class cinclass1 {
+    public static void main(String args[]){
+      int age=27;
+      System.out.println("Hello World" + age);
+    }
+
+}
---
+++ src/cinclass2.java
@@ -0,0 +1,16 @@
+public class cinclass2 {
+    public static void main(String args[]){
+
+        int num1=20, num2=15,sum,sub,mult,div;
+        sum=num1 + num2;
+        sub=num1 - num2;
+        mult=num1 * num2;
+        div=num1 / num2;
+
+        System.out.println("The result is:"+ sum + sub + mult + div);
+
+
+
+
+    }
+}
